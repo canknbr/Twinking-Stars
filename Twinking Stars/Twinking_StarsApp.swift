75cb7b627750e8076413f618a352998f24acde05
canknbr <cankanbur1@gmail.com>
2023-04-03 18:44:52 +0300

    Initial Commit

---
+++ Twinking Stars/Twinking_StarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twinking_StarsApp.swift
+//  Twinking Stars
+//
+//  Created by Can Kanbur on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Twinking_StarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
